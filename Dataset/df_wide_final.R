220719b7f6c16a88decb988806304b5508bad8d7
Sanjana Venkatesh <venkas17@mcmaster.ca>
2026-03-20 10:37:05 -0400

    Update df_wide_final.R

--- Dataset/df_wide_final.R
+++ Dataset/df_wide_final.R
@@ -1,19 +1,30 @@
 library(tidyverse)
+library(reshape2)
 
-df <- read.csv("TxS_final.csv")
+df1 <- read.csv("TxS_final.csv")
 
 perf_vars <- c("RMR", "CS", "ATPase")
+perf_vars_mass <- paste0("mass_", perf_vars)
 
-df_perf_measures <- df %>%
-  filter(va2r_name %in% perf_vars) %>%
-  select(AT, AS, TT, genotype, var_name, var_measure) %>%
-  pivot_wider(names_from = var_name, values_from = var_measure)
+df_perf_measures <- df1
+  |> filter(var_name %in% perf_vars) 
+  |>  select(AT, AS, TT, genotype, var_name, var_measure) 
+  |> pivot_wider(names_from = var_name, values_from = var_measure)
 
-df_perf_masses <- df %>%
-  filter(var_name %in% perf_vars) %>%
-  select(AT, AS, TT, genotype, var_name, mass) %>%
-  pivot_wider(names_from = var_name, values_from = mass, names_prefix = "mass_")
+df_perf_masses <- (df1 
+  |> filter(var_name %in% perf_vars)
+  |> select(AT, AS, TT, genotype, var_name, mass) 
+  |> pivot_wider(names_from = var_name, values_from = mass, names_prefix = "mass_")
+)
 
 df_final <- left_join(df_perf_measures, df_perf_masses, by = c("AT", "AS", "TT", "genotype"))
 
-saveRDS(df_final, file="df_wide_final.rds")
+df_final<- (df_final
+             |> mutate(across(c(perf_vars,perf_vars_mass), log))
+)
+
+df1_melt <- melt(df_final, id.vars = c("AT","AS","TT","genotype"), 
+                 measure.vars = c(perf_vars,perf_vars_mass),
+                 variable.name = "measure")
+
+saveRDS(df1_melt, file="df_wide_final.rds")
